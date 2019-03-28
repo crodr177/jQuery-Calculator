40b50283ea491b3a52d18f92daba5b970e8674d7
Cruz Rodriguez <cruzrodriguez1127@gmail.com>
2019-03-28 00:06:50 -0700

    Adventure Mode

--- main.js
+++ main.js
@@ -1,39 +1,46 @@
-//Explore Mode
+//Adventure Mode
 
-$(function() {
-   $("#add").on("click", function(e) {
+  let num1 = "";
+  let num2 = "";
+  let operator = "";
+  let total = "";
+
+  $("button").on("click", function (e) {
     e.preventDefault();
-
-    var num1 = Number($("#num1").val());
-    var num2 = Number($("#num2").val());
-
-    $("#answer").html(num1 + num2); 
+    var value = $(this).attr("id");
+    if(operator === "" && isNaN(value)) {
+      if ($(this).is("#clear")) {
+        num1 = "";
+        num2 = "";
+        operator = "";
+        total = "";
+        $("#answer").html("");
+      } 
+      else if ($(this).is("#equals")) {
+        return total;
+      }
+      else {
+        operator = value;
+      }
+    }
+    else if(num1 === "") {
+        num1 = Number(value);
+        $("#answer").html(value);
+      }
+    else if(num2 === "") {
+        num2 = Number(value);
+        $("#answer").html(value);
+      } 
+    else {
+      total = eval(num1 + operator + num2);
+      if(Number.isInteger(total)) {
+        $("#answer").html(total);
+      } else {
+        var roundedTotal = total.toFixed(1);
+        $("#answer").html(roundedTotal);
+      }
+      num1 = total;
+      num2 = "";
+      operator = "";
+      }
   })
-
-  $("#subtract").on("click", function(e) {
-    e.preventDefault();
-
-    var num1 = Number($("#num1").val());
-    var num2 = Number($("#num2").val());
-
-    $("#answer").html(num1 - num2); 
-  })
-
-  $("#multiple").on("click", function(e) {
-    e.preventDefault();
-
-    var num1 = Number($("#num1").val());
-    var num2 = Number($("#num2").val());
-
-    $("#answer").html(num1 * num2); 
-  })
-
-  $("#divide").on("click", function(e) {
-    e.preventDefault();
-
-    var num1 = Number($("#num1").val());
-    var num2 = Number($("#num2").val());
-
-    $("#answer").html(num1 / num2); 
-  })
-})
